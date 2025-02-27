b5523966eaac9641a82958325fff63ef0ce4807e
MisacAguilar08 <misacaguilar@gmail.com>
2025-02-26 22:00:09 -0600

    first commit

---
+++ lib/app/app.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:my_task/app/view/home.dart';
+import 'package:my_task/app/view/splash.dart';
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const primary = Color(0xFF40B7AD);
+    const textColor = Color(0xFF4A4A4A);
+    const backgroundColor = Color(0xFFF5F5F5);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: primary),
+        scaffoldBackgroundColor: backgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(
+          fontFamily: 'Poppins',
+          bodyColor: textColor,
+          displayColor: textColor
+        ),
+        useMaterial3: true,
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
